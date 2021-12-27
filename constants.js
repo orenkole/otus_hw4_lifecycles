2c639b4a00f72bbcf554c6dcf6dd66d99e4a8041
badger <badger@badger.local>
2021-12-27 17:08:14 +0200

    hw4 added workflow to deploy

--- constants.js
+++ constants.js
@@ -2,5 +2,5 @@ const {resolve} = require('path');
 const {path: PROJECT_ROOT} = require('app-root-path');
 
 exports.PROJECT_ROOT = PROJECT_ROOT;
-exports.BUILD_DIRECTORY = resolve(PROJECT_ROOT, './dist');
+exports.BUILD_DIRECTORY = resolve(PROJECT_ROOT, "./build");
 exports.SOURCE_DIRECTORY = resolve(PROJECT_ROOT, './src');
